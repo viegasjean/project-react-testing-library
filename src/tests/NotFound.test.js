7d9b6bc06f13ebe1115fbe8bf3407d301288c9f8
Jean Viegas <jeanmelg@gmail.com>
2022-01-28 18:43:54 -0300

    finaliza requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o component NotFound', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+  });
+  it('Testa se a página contem um h2 com o texto Page requested not found', () => {
+    const title = screen.getByRole('heading', { name: /page/i });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent('Page requested not found');
+  });
+  it('Testa se a página mostra a imagem', () => {
+    const image = screen.getByAltText(/pikachu/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
