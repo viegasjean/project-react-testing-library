f7adc06e09d1439537b46d63f03cbadf09c25995
Jean Viegas <jeanmelg@gmail.com>
2022-01-28 16:22:31 -0300

    finaliza requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('deve renderizar o componente App', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  expect(homeLink).toBeInTheDocument();
+  // userEvent.click(homeLink);
+
+  const homeTitle = screen.getByRole('heading', {
+    level: 1,
+    name: 'Pokédex',
+  });
+
+  expect(homeTitle).toBeInTheDocument();
+});
+
+test('deve renderizar o componente Sobre', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+
+  const aboutTitle = screen.getByRole('heading', {
+    level: 2,
+    name: 'About Pokédex',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+test('deve renderizar o componente FavoritePokemons', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+
+  const aboutTitle = screen.getByRole('heading', {
+    level: 2,
+    name: 'Favorite pokémons',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+});
