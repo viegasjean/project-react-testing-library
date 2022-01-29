d0c16713a76fd00d93d1892fefe598e3c999f79e
Jean Viegas <jeanmelg@gmail.com>
2022-01-28 22:57:51 -0300

    finaliza requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import pokemons from './favPokeMock';
+
+describe('Testa o component FavoritePokemons', () => {
+  test('se é exibido No favorite pokemon found, se não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokeNotFount = screen.getByText('No favorite pokemon found');
+    expect(pokeNotFount).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -8,12 +8,12 @@ describe('Testa o component NotFound', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/xablau');
   });
-  it('Testa se a página contem um h2 com o texto Page requested not found', () => {
+  test('se a página contem um h2 com o texto Page requested not found', () => {
     const title = screen.getByRole('heading', { name: /page/i });
     expect(title).toBeInTheDocument();
     expect(title).toHaveTextContent('Page requested not found');
   });
-  it('Testa se a página mostra a imagem', () => {
+  test('se a página mostra a imagem', () => {
     const image = screen.getByAltText(/pikachu/i);
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
